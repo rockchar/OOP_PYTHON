d59b14c682dbdae1d66bb1d642ae9c8bd28426a8
Rohit Kumar <rohitsinha050481@gmail.com>
2021-01-28 22:12:59 +0530

    Python modules

---
+++ PythonModules.py
@@ -0,0 +1,42 @@
+''' 
+here we will see some python module classes 
+'''
+
+from collections import Counter
+
+#lets create a list 
+
+my_list = [11,2,2,2,2,23,3,3,3,3,3,4,4,4,4,11,23,9,9,9,9,9]
+print(Counter(my_list))
+
+sentence = "how many times each word will appear in this sentence. the words can be counted using the counter as they appear"
+#print(sentence.split())
+
+print(Counter(sentence.split()))
+
+c= Counter(sentence)
+print(c.most_common())
+
+print(list(c))  #prints the unique elements in the sentnce 
+
+
+from collections import defaultdict
+my_def_dict = defaultdict(lambda:0)
+
+my_def_dict["Correct"] = 1000
+print(my_def_dict["Correct"])
+print(my_def_dict["Wrong"])
+
+
+from collections import namedtuple
+
+
+Dog = namedtuple("Dog",["name","age","breed"])
+
+sammy = Dog(name="Sammy",age=10,breed="Huskey")
+
+print(sammy.name)
+print(sammy.age)
+print(sammy.breed)
+
+
